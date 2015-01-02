40ce4b7ee290d99a4a42a1699c11e6443ca33a84
Jacek Miszczak <jacekmiszczak@gmail.com>
2015-01-02 20:17:36 +0100

    Util method, because too much is too much

--- src/main/groovy/com/github/fm_jm/softcomputing/Utils.groovy
+++ src/main/groovy/com/github/fm_jm/softcomputing/Utils.groovy
@@ -1,9 +1,7 @@
 package com.github.fm_jm.softcomputing
 
 import com.github.fm_jm.softcomputing.ga.GeneticAlgorithm
-import com.github.fm_jm.softcomputing.heuristics.Context
 import com.github.fm_jm.softcomputing.impl.ContextHandlerImpl
-import com.github.fm_jm.softcomputing.impl.ContextLoader
 import com.github.fm_jm.softcomputing.impl.FunctionTree
 import com.github.fm_jm.softcomputing.impl.RandomFunctionsGenerator
 import com.github.fm_jm.softcomputing.impl.StopCondImpl
@@ -17,26 +15,20 @@ import com.github.fm_jm.softcomputing.impl.sa.BoltzmannAcceptance
 import com.github.fm_jm.softcomputing.impl.sa.FastBoltzmannScheme
 import com.github.fm_jm.softcomputing.sa.SimulatedAnnealing
 
+class Utils {
 
-File file = new File(this.class.classLoader.getResource("ln+cos.csv").toURI())
+    static GeneticAlgorithm<FunctionTree> defaultGA(int popSize, int maxGen, int initCP, int stepCP, int initMP, int stepMP){
+        new GeneticAlgorithm<FunctionTree>(
+                popSize,
+                new ContextHandlerImpl(),
+                new RandomFunctionsGenerator(),
+                new StopCondImpl(maxGen),
+                new SubTreeInjectingCrossover(),
+                new AnnealingMutation(new SimulatedAnnealing<FunctionTree>(new FastBoltzmannScheme(), new BasicNeighbourSelector(), new BoltzmannAcceptance<FunctionTree>())),
+                new SelectImpl(),
+                new CPImpl(initCP, stepCP),
+                new MPImpl(initMP, stepMP)
+        )
+    }
 
-Context loaded = ContextLoader.loadFromFile(file)
-
-//def ga = new GeneticAlgorithm<FunctionTree>(
-//        100,
-//        new ContextHandlerImpl(),
-//        new RandomFunctionsGenerator(),
-//        new StopCondImpl(50),
-//        new SubTreeInjectingCrossover(),
-//        new AnnealingMutation(new SimulatedAnnealing<FunctionTree>(new FastBoltzmannScheme(), new BasicNeighbourSelector(), new BoltzmannAcceptance<FunctionTree>())),
-//        new SelectImpl(),
-//        new CPImpl(750, 10),
-//        new MPImpl(200, 10)
-//)
-
-def ga = Utils.defaultGA(100, 50, 750, 10, 200, 10)
-
-def pop = ga.doRun(loaded)
-
-println pop[0]
-println pop[0].evaluate(loaded)
+}
