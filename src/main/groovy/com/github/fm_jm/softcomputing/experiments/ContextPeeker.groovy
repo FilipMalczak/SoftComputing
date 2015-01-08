e3280eccfa20b2d5265e726f01f8246bf5a8fb07
Filip Malczak <filip.malczak@gmail.com>
2015-01-08 12:22:30 +0100

    Results batch

--- src/main/groovy/com/github/fm_jm/softcomputing/experiments/ContextPeeker.groovy
+++ src/main/groovy/com/github/fm_jm/softcomputing/experiments/ContextPeeker.groovy
@@ -9,11 +9,14 @@ import static com.github.fm_jm.softcomputing.experiments.impl.Utils.fromCSVKey
 class ContextPeeker implements Runnable{
 
 //    String key = "exp.csv;0;0.05;2;100.0;1.0;3;2;10;400;400;10000.0;100;50;800;10;300;10"
-//    String key = "CURRENT"
-    String key = fromCSVKey "exp,csv\t0\t0,05\t2\t100,0\t1,0\t3\t2\t10\t400\t400\t10000,0\t100\t50\t800\t10\t300\t10"
+    String key = "CURRENT"
+//    String key = fromCSVKey "exp,csv\t0\t0,05\t2\t100,0\t1,0\t3\t2\t10\t400\t400\t10000,0\t100\t50\t800\t10\t300\t10"
 
     @Override
     void run() {
-        println new JsonBuilder( ResultStorage.instance.load(key) ).toPrettyString()
+        def ctx = ResultStorage.instance.load(key)
+        println ctx.globalBest.toString()
+        println ctx.globalBest.evaluate(ctx)
+        println new JsonBuilder( ctx ).toPrettyString()
     }
 }
